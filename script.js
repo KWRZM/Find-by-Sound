7659279ceddb8a3ec646f582bdbe430b876b6c6d
KWRZM <84276353+KWRZM@users.noreply.github.com>
2023-06-25 03:03:35 +0100

    Added difficulties system

--- script.js
+++ script.js
@@ -1,13 +1,32 @@
 let audio = document.getElementById("audio");
+let leBout = document.querySelector('.elem');
 
-
-function start(){
+function start(difficulty){
     audio.play();
-    document.querySelector('.play-button').style.display = "none";
+    document.querySelector('.diff').style.display = "none";
+    leBout.style.height = difficulty + "px";
+    leBout.style.width = difficulty + "px";
+
+
+    document.addEventListener('mousemove', function(e) {
+
+        var distance = parseInt(Math.sqrt(Math.pow((leBout.offsetLeft) - e.x, 2) + Math.pow((leBout.offsetTop) - e.y, 2)));
+        let pourcentage = (distance / diagonale).toFixed(4);
+        
+        audio.volume =  1 - pourcentage;
+        audio.playbackRate = (1 - pourcentage) + 0.5; 
+        if( 1 - pourcentage > 0.90)audio.playbackRate = audio.playbackRate + audio.playbackRate * 0.3;
+        if( 1 - pourcentage > 0.95)audio.playbackRate = audio.playbackRate + audio.playbackRate * 0.3;
+        if( 1 - pourcentage > 0.98)audio.playbackRate = audio.playbackRate + audio.playbackRate * 0.3;
+    
+        console.log(pourcentage + "%");
+        console.log("volume: " + audio.volume);
+        console.log("Vitesse: " + audio.playbackRate)
+    
+    });
 }
 
 
-let leBout = document.querySelector('.elem');
 
 var largeurPage = window.innerWidth || document.documentElement.clientWidth;
 var hauteurPage = window.innerHeight || document.documentElement.clientHeight;
@@ -25,19 +44,7 @@ leBout.style.top = `${y}px`;
 leBout.style.left = `${x}px`;
 console.log(leBout);
 
-document.addEventListener('mousemove', function(e) {
-    //console.log("X: " + e.x + " Y: " + e.y);
-    var distance = parseInt(Math.sqrt(Math.pow((leBout.offsetLeft) - e.x, 2) + Math.pow((leBout.offsetTop) - e.y, 2)));
-    let pourcentage = (distance / diagonale).toFixed(4);
-    
-    audio.volume =  1 - pourcentage;
-    audio.playbackRate = (1 - pourcentage) + 0.5; 
 
-    console.log(pourcentage + "%");
-    console.log("volume: " + audio.volume);
-    console.log("Vitesse: " + audio.playbackRate)
-
-});
 
 leBout.addEventListener('click',function(e){
     audio.pause();
